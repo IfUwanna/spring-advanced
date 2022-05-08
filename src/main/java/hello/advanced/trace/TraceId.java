ecb6c2a5ab0f01ea0d2adf83035d43e143b59b39
jihunPark <ongamenet87@gmail.com>
2022-05-08 23:45:08 +0900

    220509-Trace-V0

---
+++ src/main/java/hello/advanced/trace/TraceId.java
@@ -0,0 +1,44 @@
+package hello.advanced.trace;
+
+import java.util.UUID;
+
+public class TraceId {
+    private String id;
+    private int level;
+
+    public TraceId() {
+          this.id = createId();
+          this.level = 0;
+    }
+
+    private String createId() {
+        return UUID.randomUUID().toString().substring(0,8);
+    }
+
+    public TraceId createNextId() {
+        return new TraceId(id, level + 1); // 동일 id에 level만 하나 증가!
+    }
+
+    public TraceId createPreviousId() {
+        return new TraceId(id, level + 1); // 동일 id에 level만 하나 감소!
+    }
+
+    public boolean isFirestLevel(){
+        return level==0;
+    }
+
+    public TraceId(String id, int level) {
+        this.id = id;
+        this.level = level;
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+}
