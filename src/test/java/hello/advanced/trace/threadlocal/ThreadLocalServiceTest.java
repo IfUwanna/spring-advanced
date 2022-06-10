006b5cd0fc2adf799413b0b7efdfa064dc4c32d3
jihunPark <ongamenet87@gmail.com>
2022-06-10 15:10:35 +0900

    220610-TraceLogV3-ThreadLocal 예제

---
+++ src/test/java/hello/advanced/trace/threadlocal/ThreadLocalServiceTest.java
@@ -0,0 +1,48 @@
+package hello.advanced.trace.threadlocal;
+
+import hello.advanced.trace.threadlocal.code.FieldService;
+import hello.advanced.trace.threadlocal.code.ThreadLocalService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ThreadLocalServiceTest {
+
+    private ThreadLocalService threadLocalService = new ThreadLocalService();
+
+    @Test
+    public void test() throws Exception{
+        log.info("main start");
+        Runnable userA = new Runnable() {
+            @Override
+            public void run() {
+                threadLocalService.logic("userA");
+            }
+        };
+
+        Runnable userB = () -> {
+            threadLocalService.logic("userB");
+        };
+
+        Thread threadA = new Thread(userA);
+        threadA.setName("thread-A");
+        Thread threadB = new Thread(userB);
+        threadB.setName("thread-B");
+
+        threadA.start();
+        //sleep(2000); // 동시성 문제 발생 X
+        sleep(100); // 동시성 문제 발생 O
+        threadB.start();
+
+        sleep(3000); // 메인쓰레드 종료 대기
+        log.info("main exit");
+    }
+
+    private void sleep(int millis){
+        try{
+            Thread.sleep(millis);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+}
